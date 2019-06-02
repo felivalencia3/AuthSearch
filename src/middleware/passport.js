0efe732bd219bcfa90233200bd28ea83a2a83451
Felipe Valencia <fvalencia@vc-soft.com>
2019-06-01 20:14:18 -0500

    Enforced HTTPS

--- src/middleware/passport.js
+++ src/middleware/passport.js
@@ -9,18 +9,18 @@ const authconfig = (req, res, next) => {
   passport.use(new FacebookStrategy({
     clientID: config.facebookID,
     clientSecret: config.facebookSecret,
-    callbackURL: '/api/facebook/callback',
+    callbackURL: 'https://authsearch.herokuapp.com/api/facebook/callback',
     profileFields: ['id', 'displayName', 'email'],
   }, ((accessToken, refreshToken, profile, cb) => cb(null, profile))));
   passport.use(new GithubStrategy({
     clientID: config.githubID,
     clientSecret: config.githubSecret,
-    callbackURL: '/api/github/callback',
+    callbackURL: 'https://authsearch.herokuapp.com/api/github/callback',
   }, ((accessToken, refreshToken, profile, cb) => cb(null, profile))));
   passport.use(new SpotifyStrategy({
     clientID: config.spotifyID,
     clientSecret: config.spotifySecret,
-    callbackURL: '/api/spotify/callback',
+    callbackURL: 'https://authsearch.herokuapp.com/api/spotify/callback',
     profileFields: ['id', 'displayName', 'email'],
   }, ((accessToken, refreshToken, profile, cb) => cb(null, profile))));
   passport.serializeUser((user, cb) => {
